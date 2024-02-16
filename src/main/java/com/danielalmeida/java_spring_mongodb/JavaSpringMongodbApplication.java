5dcede15d7fa2d84ea2308f5dd5004b5c80e82d8
daniel-liberto <daniel.liberto@hotmail.com>
2024-02-16 17:16:03 -0300

    first commit

---
+++ src/main/java/com/danielalmeida/java_spring_mongodb/JavaSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.danielalmeida.java_spring_mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielalmeida/java_spring_mongodb/JavaSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielalmeida.java_spring_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
